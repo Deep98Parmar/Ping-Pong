9e1e0906693617e0cbe6881e7588986e595edaba
Deep Parmar <deep98parmar@yahoo.com>
2018-02-27 08:08:04 +0300

    the css code

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+funtion pingpong(num) {
+  var myArray=[];
+  for (var a =1; a<=num; a++) {
+    if (a % 15 === 0){
+      myArray.push("PING PONG");
+    }else if (a % 5=== 0){
+myArray.push("pong");}
+}else if (a % 3=== 0){
+myArray.push("ping");}
+else {myArray.push(a);
+}
+};
+return myArray;
+};
+$(document).ready(function(){
+  $("form#form-num").submit(function(event)
+}
+event.preventDefault();
+$("li.res").empty();
+var num=parseInt($("#number-input").val());
+var derivedArray= pingpong(num) +'';
+var splitArray=
+derivedArray.split(",").join("<br");
+$("div.result-
+table").addClass('res').append("<li
+class='res'>"+ splitArray+"</li>");
+});
+});
